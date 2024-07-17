3ce26262401815fddcf13a54130edc95ac959457
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-07-17 18:25:06 +0200

    update workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-      "node": true,
-      "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-      // Ajoutez vos règles ESLint personnalisées ici
-      // Exemple :
-      // "semi": ["error", "always"],
-      // "quotes": ["error", "single"],
-      // "indent": ["error", 2]
-    }
-  };
-  
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    // Ajoutez vos règles ESLint personnalisées ici
+    // Exemple :
+    // "semi": ["error", "always"],
+    // "quotes": ["error", "single"],
+    // "indent": ["error", 2]
+  },
+};
