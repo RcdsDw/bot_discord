09eaee19720040e2060872e5548943c3e2664ff3
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-07-17 17:15:49 +0200

    update condition check presence

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function checkPresence(userId, guild) {
   const member = guild.members.cache.get(userId);
   if (member) {
     const presenceStatus = member.presence?.status;
-    return presenceStatus === 'online';
+    return presenceStatus !== 'online';
   }
   return false;
 }
