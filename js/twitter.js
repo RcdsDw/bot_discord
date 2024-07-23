5e4b5c12467691248cf31336a80a79ae3391dbde
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-07-23 23:50:29 +0200

    add count for twitter js

--- js/twitter.js
+++ js/twitter.js
@@ -1,6 +1,8 @@
 function Twitter(msg) {
   const twitterReg = /^https:\/\/x\.com/;
-  if (msg.match(twitterReg)) {
+  let count = 0;
+  if (msg.match(twitterReg) && count === 0) {
+    count++;
     const newMsg = msg.replace(twitterReg, 'https://fxtwitter.com');
     return newMsg;
   }
