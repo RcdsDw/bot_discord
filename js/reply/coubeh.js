c0f1c5369b4b02ca1e27ae1e7ed1ccc5a766f287
RcdsDw <rafael.candidodasilva.pro@gmail.com>
2024-07-31 15:01:57 +0200

    try to add custom commands

--- js/reply/coubeh.js
+++ js/reply/coubeh.js
@@ -1,8 +1,8 @@
 function Coubeh(msg) {
-  const quiReg = /\b(qui|ki)(?=\s?.*[?¿]|$)/i;
-  const quoiReg = /\b(quoi|koi|kwa|qwa|koua|quoua|qouwa|kouwa)(?=\s?.*[?¿]|$)/i;
-  const pourQuiReg = /\b(pour qui|pour ki)(?=\s?.*[?¿]|$)/i;
-  const pourQuoiReg = /\b(pour quoi|pour koi|pourquoi|pourkoi)(?=\s?.*[?¿]|$)/i;
+  const quiReg = /\b( qui | ki )(?=\s?.*[?¿]|$)/i;
+  const quoiReg = /\b( quoi | koi | kwa | qwa | koua | quoua | qouwa | kouwa )(?=\s?.*[?¿]|$)/i;
+  const pourQuiReg = /\b( pour qui | pour ki | pour qui | pourki )(?=\s?.*[?¿]|$)/i;
+  const pourQuoiReg = /\b( pour quoi | pour koi | pourquoi | pourkoi )(?=\s?.*[?¿]|$)/i;
 
   if (msg.match(pourQuoiReg)) {
     return 'Pour coubeh je pense mon reuf !';
