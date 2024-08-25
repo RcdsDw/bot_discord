cda898d1d9940b1c798903a88d7e20065b1cfdb3
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-08-25 22:04:09 +0200

    up

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,22 +1,20 @@
 import { REST, Routes } from 'discord.js';
 import { getCommands } from './lib/command';
+import { config } from 'dotenv';
 
-// Vérifiez si les variables d'environnement sont définies
+config();
+
 const token = process.env.TOKEN_BOT ?? '';
 const clientId = process.env.CLIENT_ID ?? '';
 
-// Obtenez les commandes et convertissez-les en JSON
 const commands: string[] = getCommands().map((command: any) => command.data.toJSON());
 
-// Instanciez le module REST
 const rest = new REST({ version: '10' }).setToken(token);
 
-// Déployez les commandes
 (async () => {
   try {
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    // Utilisez la méthode PUT pour rafraîchir toutes les commandes dans la guilde
     const data_commands: any = await rest.put(
       Routes.applicationCommands(clientId),
       { body: commands },
@@ -24,7 +22,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 
     console.log(`Successfully reloaded ${data_commands.length} application (/) commands.`);
   } catch (error) {
-    // Attrapez et loguez toutes les erreurs
     console.error('Error deploying commands:', error);
   }
 })();
