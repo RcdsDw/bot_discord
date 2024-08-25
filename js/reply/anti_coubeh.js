e3cf13ab51cfb6863f77b5e523f4b66380f7e608
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-08-25 15:35:23 +0200

    issue#1 add /i in all regex

--- js/reply/anti_coubeh.js
+++ js/reply/anti_coubeh.js
@@ -1,7 +1,7 @@
 async function AntiCoubeh(msg) {
-  const coubehReg = /\bcoubeh(?=\s|$)/;
-  const ketteReg = /\b(kette|quette|quete|kete)(?=\s|$)/;
-  const feurReg = /\bfeur(?=\s|$)/;
+  const coubehReg = /\bcoubeh(?=\s|$)/i;
+  const ketteReg = /\b(kette|quette|quete|kete)(?=\s|$)/i;
+  const feurReg = /\bfeur(?=\s|$)/i;
 
   if (msg.match(coubehReg)) {
     return 'À ton âge tu dis "coubeh" ? Ressaisis toi !';
