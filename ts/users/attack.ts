0e7c011fa429e4c30e637d5ae0c4cfb490044be7
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-08-28 22:29:32 +0200

    fix attack

--- ts/users/attack.ts
+++ ts/users/attack.ts
@@ -3,16 +3,5 @@ import ryan from '../../datas/ryan.json';
 import trashV2 from '../../datas/trashV2.json';
 
 export async function Attack(interaction: any) {
-  const reply = `${userMention("113722828695535623")} Oh ${ryan[Math.floor(Math.random() * ryan.length)]}, ${trashV2[Math.floor(Math.random() * trashV2.length)]} !`;
-  const replyMessage = await interaction.reply({
-    content: reply,
-  });
-  replyMessage.react("🅶");
-  replyMessage.react("🆁");
-  replyMessage.react("🅾");
-  replyMessage.react("🆂");
-  replyMessage.react("🅵");
-  replyMessage.react("🅳");
-  replyMessage.react("🅿");
-  return;
+   await interaction.reply(`${userMention("113722828695535623")} Oh ${ryan[Math.floor(Math.random() * ryan.length)]}, ${trashV2[Math.floor(Math.random() * trashV2.length)]} !`);
 }
