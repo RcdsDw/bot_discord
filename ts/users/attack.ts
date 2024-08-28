44661b79906bd60c3c46d56d0157baa5e0ca589b
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2024-08-28 21:50:27 +0200

    add react to attack

--- ts/users/attack.ts
+++ ts/users/attack.ts
@@ -1,8 +1,16 @@
 import { userMention } from 'discord.js';
 import ryan from '../../datas/ryan.json';
 
+const emojis = ["🅶", "🆁", "🅾", "🆂", "🅵", "🅳", "🅿"]
+
 export async function Attack(interaction: any) {
-  interaction.reply(
-    `${userMention("113722828695535623")} Oh ${ryan[Math.floor(Math.random() * ryan.length)]}, nique ta mère de là !`,
-  );
+  const reply = `${userMention("113722828695535623")} Oh ${ryan[Math.floor(Math.random() * ryan.length)]}, nique ta mère de là !`;
+  const replyMessage = await interaction.reply({
+    content: reply,
+  });
+
+  for (let i = 0; i < emojis.length; i++) {
+    replyMessage.react(`${emojis[i]}`);
+  }
+  return;
 }
